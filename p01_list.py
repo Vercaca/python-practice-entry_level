315d3224f996387d72a272f8b3e4582279e5952b
Vercaca <verca2510@iir.csie.ncku.edu.tw>
2018-12-02 02:59:00 +0800

    added #3. [] vs. list()

--- p01_list.py
+++ p01_list.py
@@ -12,6 +12,7 @@ print('Original list a: {}'.format(a))
 # 1. slice [X:X]
 print('''\n\
 ------------------------
+ #1
  LIST 的 SLICE 用法 a[x:y]
  insert / delete / remove
 ------------------------\
@@ -36,6 +37,7 @@ print('After clear the list: \n{}'.format(a))
 # 2. mutable object
 print('''\n\
 ------------------------
+ #2
  MUTABLE OBJECT -- LIST
  以變數之地址回傳給對象
 ------------------------\
@@ -53,3 +55,37 @@ c = a
 c.append(4)
 print('\n- List is a mutable object, "c" points to "a" \n{}'.format(b))
 print('In this way, list "a" will be changed, a: \n{}'.format(a))
+
+
+# 3. [] vs. list()
+print('''\n\
+------------------------
+ #3
+ list() vs. []
+ 效能與pitfalls
+------------------------\
+''')
+a = get_original_a()
+
+print('\n3.1 效能')
+from timeit import timeit
+print('Time used of list(): {}'.format(timeit("list()")))
+print('Time used of []: {}'.format(timeit("[]")))
+print('[CONCLUSION] [] faster than list()')
+
+print('\n3.2 用途')
+print('• list() creates list from the same iterable')
+print('• [] stores the iterable object')
+print('\n3.2.1 pitfall 01 -- with dict(sth)')
+sth = [(1, 2), (3, 4), (5, 6)]
+sth2 = list(dict(sth))
+print('sth2 = list(dict(sth)), result: {}'.format(sth2))
+sth3 = [dict(sth)]
+print('sth3 = [dict(sth)], result: {}'.format(sth3))
+
+print('\n3.2.2 pitfall 02 -- with map(sth)')
+sth = [(1, 2), (3, 4), (5, 6)]
+sth2 = list(map(lambda x:x[1], sth))
+print('sth2 = list(map(lambda x:x[1], sth))), result: {}'.format(sth2))
+sth3 = [map(lambda x:x[1], sth)]
+print('sth3 = [map(lambda x:x[1], sth)], result: {}'.format(sth3))
